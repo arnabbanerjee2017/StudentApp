1701ea5bc5da6d408481011c2a948423d00de640
Arnab Banerjee <arnab.ban09@gmail.com>
2019-07-23 02:53:23 +0530

    Added header comments.

--- src/test/java/com/arnab/student/StudentAppApplicationTests.java
+++ src/test/java/com/arnab/student/StudentAppApplicationTests.java
@@ -1,3 +1,12 @@
+/*
+ * Copyright (c) 2019, ARNAB BANERJEE. All rights reserved.
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted only for academic purposes.
+ * 
+ * For further queries / info: arnab.ban09@gmail.com
+ */
+
 package com.arnab.student;
 
 import org.junit.Test;
